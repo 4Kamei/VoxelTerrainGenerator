9d9d9391ede527c3e828958cace5e0ebd0bb5ed5
Aleksander Kaminski <theinfamouspig@gmail.com>
2016-07-02 12:21:53 +0100

    Player Look vector + position mostly fixed

--- src/main/java/ak/physSim/entity/Player.java
+++ src/main/java/ak/physSim/entity/Player.java
@@ -1,5 +1,7 @@
 package ak.physSim.entity;
 
+import ak.physSim.util.Logger;
+import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import org.joml.Vector3i;
 import org.lwjgl.glfw.GLFW;
@@ -10,7 +12,7 @@ import org.lwjgl.glfw.GLFW;
 public class Player {
     private Vector3f lookVector;
     private Vector3f position;
-    private final float speed = 0.1f;
+    private final float speed = 0.01f;
     private boolean up, down, left, right;
     private float azimuth, pitch;
 
@@ -23,21 +25,28 @@ public class Player {
         up = down = left = right = false;
         if (key == GLFW.GLFW_KEY_UP || key == GLFW.GLFW_KEY_W)
             up = (action != 0);
-        if (key == GLFW.GLFW_KEY_DOWN|| key == GLFW.GLFW_KEY_S);
+        if (key == GLFW.GLFW_KEY_DOWN|| key == GLFW.GLFW_KEY_S)
             down = (action != 0);
-        if (key == GLFW.GLFW_KEY_LEFT|| key == GLFW.GLFW_KEY_A);
+        if (key == GLFW.GLFW_KEY_LEFT|| key == GLFW.GLFW_KEY_A)
             left = (action != 0);
-        if (key == GLFW.GLFW_KEY_RIGHT|| key == GLFW.GLFW_KEY_D);
+        if (key == GLFW.GLFW_KEY_RIGHT|| key == GLFW.GLFW_KEY_D)
             right = (action != 0);
     }
 
-    public void update(int delta){
-
-        if (up){
-            position.add(
-                    (float) -Math.sin(lookVector.y) * delta * speed,
-                    0,
-                    (float) Math.cos(lookVector.y) * delta * speed);
+    public void update(int delta) {
+        float update = delta*speed;
+        if (up) {
+            position = position.add(lookVector.x * update, lookVector.y * update, lookVector.z * update);
+        }
+        if (down) {
+            position = position.sub(lookVector.x * update, lookVector.y * update, lookVector.z * update);
+        }
+        if (right)
+            position.zero();
+        if (left){
+            Logger.log(Logger.LogLevel.DEBUG, String.format("POS: %.3f, %.3f, %.3f", position.x, position.y, position.z));
+            Logger.log(Logger.LogLevel.DEBUG, String.format("NORM POS %.3f, %.3f, %.3f",position.x/position.length(), position.y/position.length(), position.z/position.length()));
+            Logger.log(Logger.LogLevel.DEBUG, String.format("EYE: %.3f, %.3f, %.3f", lookVector.x, lookVector.y, lookVector.z));
         }
     }
 
@@ -45,15 +54,24 @@ public class Player {
         return lookVector;
     }
 
-    public void setLook(float azimuth, float pitch){
-        this.azimuth = azimuth;
-        this.pitch = pitch;
-        this.lookVector = caluclateLookVector();
-    }
+        public void setLook(float azimuth, float pitch){
+            this.azimuth = (float) ((azimuth + Math.PI * 2) % (Math.PI * 2));
+            this.pitch = (float) (((pitch) + Math.PI/2 +  Math.PI*2) % (Math.PI * 2));
+            caluclateLookVector();
+        }
 
-    public Vector3f caluclateLookVector() {
-        float sinPitch = (float) Math.sin(pitch);
-        return new Vector3f((float) (sinPitch*Math.cos(azimuth)), (float) Math.cos(pitch), (float) -(sinPitch*Math.sin(pitch)));
+    private void caluclateLookVector() {
+        float sinPitch = (float) Math.sin(-pitch);
+        /*
+        Matrix4f matrix4f = new Matrix4f().identity();
+        matrix4f.rotateX(pitch);
+        Logger.log(Logger.LogLevel.DEBUG, matrix4f.toString());
+        Logger.log(Logger.LogLevel.DEBUG, matrix4f.m22 + "");
+        Vector3f lookVector = new Vector3f(matrix4f.m20, matrix4f.m21, matrix4f.m22);*/
+        lookVector = new Vector3f((float) (-sinPitch*Math.sin(azimuth)), (float) Math.cos(pitch), (float) (sinPitch*Math.cos(azimuth)));
+        Logger.log(Logger.LogLevel.DEBUG, String.format("EYE: %.3f, %.3f, %.3f", lookVector.x, lookVector.y, lookVector.z));
+        Logger.log(Logger.LogLevel.DEBUG, String.format("AXS: %d, %d, %d", getAxisVector().x, getAxisVector().y, getAxisVector().z));
+
     }
 
     public Vector3i getAxisVector(){
@@ -65,6 +83,14 @@ public class Player {
     }
 
     public Vector3f getTransform() {
-        return position.negate();
+        return position;
+    }
+
+    public float getAzimuth() {
+        return azimuth;
+    }
+
+    public float getPitch() {
+        return (float) (pitch - Math.PI/2);
     }
 }
