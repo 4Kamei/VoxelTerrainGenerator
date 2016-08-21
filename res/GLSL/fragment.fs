f49aa2cc122628fb6f069ec66577404cfe7234cb
Aleksander Kaminski <theinfamouspig@gmail.com>
2016-08-21 12:27:07 +0100

    voxelLighting works. Code needs cleanup

--- res/GLSL/fragment.fs
+++ res/GLSL/fragment.fs
@@ -3,8 +3,8 @@
 in vec3 exColour;
 in vec3 vertexPos;
 in vec3 vertexNormal;
-
-
+in vec3 localVertPos;
+in vec3 localNormal;
 struct AreaLight {
                    vec3 colIntensities;
                    vec3 position;
@@ -16,20 +16,24 @@ struct DirectionalLight {
                     vec3 colIntensity;
                     float power;
 };
-uniform AreaLight light;
 
-uniform mat4 model;
+uniform int voxelLight[4096]; //TODO: pass from vertex shader instead
+
+uniform AreaLight light;
 
 out vec4 fragColor;
 
-void main(void)
+void main()
 {
-     //Removing the scaling breaks it
+    vec3 vertPos = localVertPos + localNormal;
+    int index = int(vertPos.x) + int(vertPos.y)*16+ int(vertPos.z)*256;
+    int lightIntensity = voxelLight[index];
+
     vec3 normal = vertexNormal;
     vec3 surfaceToLight = light.position - vertexPos;
 
     float brightness = light.power *  dot(normal, surfaceToLight)/ (length(normal) * length(surfaceToLight) * length(surfaceToLight));
     brightness = clamp(brightness, 0, 1);
-    fragColor = vec4((brightness) * light.colIntensities * exColour, 1.0);
+    fragColor = vec4((brightness * 0.001 + 0.500 * lightIntensity/16 + 0.499) * light.colIntensities * exColour, 1.0);
 
 }
--- res/GLSL/vertex.vs
+++ res/GLSL/vertex.vs
@@ -11,9 +11,13 @@ uniform mat4 view;
 out vec3 exColour;
 out vec3 vertexPos;
 out vec3 vertexNormal;
+out vec3 localVertPos;
+out vec3 localNormal;
 
 void main()
 {
+    localNormal = inNormal;
+    localVertPos = pos;
     exColour = inColour;
     vertexNormal = vec3(model * vec4(inNormal, 0));
     vertexPos = vec3(model * vec4(pos, 1));
